bd0db812516da78773d8a6708018d54ed9988b30
sreemaye <sreemaye44@gmail.com>
2023-05-30 13:09:25 +0600

    api called

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Problem2 = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [unFilteredNumbers, setUnFilteredNumbers] = useState([]);
+  const [showEvenOnly, setShowEvenOnly] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    return (
+  useEffect(() => {
+    fetch("https://contact.mediusware.com/api/contacts/")
+      .then((response) => response.json())
+      .then((data) => {
+        const phone = data.results;
+        setNumbers(phone);
+        console.log(numbers);
+        setUnFilteredNumbers(phone);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            type="button"
+            className="btn btn-lg btn-outline-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            All Contacts
+          </button>
+          <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    Modal title
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
                 </div>
-                
+                <div className="modal-body">...</div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
             </div>
+          </div>
+
+          <button className="btn btn-lg btn-outline-warning" type="button">
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
